320f7105d65a129e1c0df6b0f88b9f785aa5e117
Charles Jolley <charles@sproutit.com>
2010-03-04 11:28:14 -0800

    seed-server cometh

---
+++ packages/seed-server/lib/index.js
@@ -0,0 +1,5 @@
+// ==========================================================================
+// Project:   Seed - Flexible Package Manager
+// Copyright: ©2009-2010 Apple Inc. All rights reserved.
+// License:   Licened under MIT license (see __preamble__.js)
+// ==========================================================================
