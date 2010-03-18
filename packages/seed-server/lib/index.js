bddcbbdb6f54d2c8af68b67c8565f3a068c9e0a8
Charles Jolley <charles@sproutit.com>
2010-03-18 01:53:38 -0700

    Remove seed-server.  Now lives as its own package

--- packages/seed-server/lib/index.js
+++
@@ -1,8 +0,0 @@
-// ==========================================================================
-// Project:   Seed - Flexible Package Manager
-// Copyright: ©2009-2010 Apple Inc. All rights reserved.
-// License:   Licened under MIT license (see __preamble__.js)
-// ==========================================================================
-
-var server = require('server');
-exports.start = server.start;
